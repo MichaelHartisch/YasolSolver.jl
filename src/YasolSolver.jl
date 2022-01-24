c873f8100dd2cac36ea2f8f705b40dcb33ea5fca
Hendrik Becker <hendrik.becker7@gmail.com>
2022-01-24 13:34:19 +0100

    Changed Tests

--- src/YasolSolver.jl
+++ src/YasolSolver.jl
@@ -1,5 +1,10 @@
 module YasolSolver
 
 # Write your package code here.
+function test(number)
+    return number*2
+end
+
+export test
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "YasolSolver.jl" begin
     # Write your tests here.
+    @test test(2) == 4
 end
